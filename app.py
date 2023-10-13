741c4d792a19a435483693c0177d45d1032d99d6
Patricio Bustamante <130309115+patob2000@users.noreply.github.com>
2023-10-13 02:18:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, Mundo!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
-  
+print("hello")
